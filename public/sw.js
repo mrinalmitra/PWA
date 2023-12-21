3f5017c3b7eacaf61a65c927bc3d5fffc760e38e
mrinalgithub <mrinalgithub@gmail.com>
2023-12-21 13:25:54 +0530

    chore: static service worker caching

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,26 @@
 self.addEventListener('install',function(event){
     console.log('[Service Worker] Installing Service Worker ...',event)
+    event.waitUntil(
+        caches.open('static')
+        .then(function(cache){
+            console.log('[Service worker] Precaching App Shell');
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/src/js/app.js',
+                '/src/js/feed.js',
+                '/src/js/promise.js',
+                '/src/js/fetch.js',
+                '/src/js/material.min.js',
+                '/src/css/app.css',
+                '/src/css/feed.css',
+                '/src/images/main-image.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+              ]);
+        })
+    )
 });
 
 self.addEventListener('activate',function(event){
@@ -8,5 +29,14 @@ self.addEventListener('activate',function(event){
 })
 
 self.addEventListener('fetch',function(event){
-    event.respondWith(fetch(event.request))
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(res){
+            if(res){
+                return res
+            }else{
+                return fetch(event.request)
+            }
+        })  
+    );
 })
